b27bf83b4b4c079fbffe4f7e44eca112dcf1f0ac
koka687 <annzav1904@gmail.com>
2025-04-02 00:11:48 +0300

    shows templates created

---
+++ demo/src/main/templates/show/product/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("product-id").textContent = product.id;
+    document.getElementById("product-name").textContent = product.name;
+    document.getElementById("product-produced").textContent = product.produced;
+    document.getElementById("product-sold").textContent = product.sold;
+    document.getElementById("product-date").textContent = product.dateMade;
+});
